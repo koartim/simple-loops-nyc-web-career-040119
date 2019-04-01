4975a1f803aa23a142f22651bcede5977dd4f33b
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-04-01 17:15:11 -0400

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,59 +1,44 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+phrase = "Welcome to Flatiron School's Web Development Course!"
 counter = 0
 loop do
-  counter = counter + 1
-  puts "Welcome to Flatiron School's Web Development Course!"
-  if counter > 6
-    break
-  end
-end
-
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  puts phrase
+  counter += 1
+  break if counter == number_of_times
 end
 
 def times_iterator(number_of_times)
-  7.times do
-    puts "Welcome to Flatiron School's Web Development Course!"
-  end
-
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  number_of_times.times do
+  puts phrase
+  end
 end
 
 def while_iterator(number_of_times)
-  number_of_times = 0
-    while number_of_times < 7
-      number_of_times += 1
-      puts "Welcome to Flatiron School's Web Development Course!"
-
-  end
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+phrase = "Welcome to Flatiron School's Web Development Course!"
+counter = 0
+while counter < number_of_times
+  puts phrase
+  counter += 1
+end
 end
 
 def until_iterator(number_of_times)
-  counter = 0
-  until counter == 7
-    puts "Welcome to Flatiron School's Web Development Course!"
-    counter += 1
-
-  end
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  counter = 0
+  until counter == number_of_times
+    puts phrase
+    counter += 1
+  end
 end
 
 def for_iterator(number_of_times)
-  number_of_times = 1..7
-  for number in number_of_times
-    puts "Welcome to Flatiron School's Web Development Course!"
-  end
-  
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
-
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+phrase = "Welcome to Flatiron School's Web Development Course!"
+counter = 0
+for number_of_times in (1..7)
+  puts phrase
+end
+end
 end
